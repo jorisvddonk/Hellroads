3f6a2bfaae83bb197c636d9fe9b6bf7a5ad63d2b
joris <jorisvddonk@gmail.com>
2019-09-03 21:28:31 +0200

    Generate level from image

---
+++ test.ts
@@ -0,0 +1,274 @@
+import * as fs from "fs";
+import * as WAD from "node-wad";
+import Jimp from "jimp";
+import { Jimp as IJimp } from "jimp";
+import {
+  Sector,
+  generateUDMF,
+  Sidedef,
+  Linedef,
+  Vertex,
+  reffables,
+  RefType
+} from "./udmf_types";
+
+const getSpriteName = (color: number) => {
+  const rgba = Jimp.intToRGBA(color);
+  const getCol = i =>
+    Math.floor(i / 16)
+      .toString(16)
+      .toUpperCase();
+  return `0${getCol(rgba.r)}${getCol(rgba.g)}${getCol(rgba.b)}`;
+};
+
+interface Pixel {
+  x: number;
+  y: number;
+  XS: number;
+  YS: number;
+  color: number;
+  sprite: string;
+  lin_N?: Linedef;
+  lin_E?: Linedef;
+  lin_S?: Linedef;
+  lin_W?: Linedef;
+  own_N?: boolean;
+  own_E?: boolean;
+  own_S?: boolean;
+  own_W?: boolean;
+  border_N?: boolean;
+  border_E?: boolean;
+  border_S?: boolean;
+  border_W?: boolean;
+  sector?: Sector;
+  sid_N?: Sidedef;
+  sid_E?: Sidedef;
+  sid_S?: Sidedef;
+  sid_W?: Sidedef;
+}
+
+const pixels: Pixel[] = [];
+const getPixel = (x: number, y: number) => {
+  return pixels.find(p => p.x === x && p.y === y);
+};
+
+const generateStuffFromimage = function(img: IJimp) {
+  const sector0 = generateRoom(
+    new Vertex(
+      -img.bitmap.width * SCALE * 1.25,
+      img.bitmap.height * SCALE * 1.25
+    ),
+    new Vertex(
+      img.bitmap.width * SCALE * 1.25,
+      img.bitmap.height * SCALE * 1.25
+    ),
+    new Vertex(
+      img.bitmap.width * SCALE * 1.25,
+      -img.bitmap.height * SCALE * 1.25
+    ),
+    new Vertex(
+      -img.bitmap.width * SCALE * 1.25,
+      -img.bitmap.height * SCALE * 1.25
+    ),
+    "0000"
+  );
+
+  img.scanQuiet(0, 0, img.bitmap.width, img.bitmap.height, (x, y, idx) => {
+    const color = img.getPixelColor(x, y);
+    const sprite = getSpriteName(color);
+    const X = x - img.bitmap.width / 2;
+    const Y = img.bitmap.height / 2 - y;
+    const XS = X * SCALE;
+    const YS = Y * SCALE;
+
+    pixels.push({
+      x: x,
+      y: y,
+      XS,
+      YS,
+      color,
+      sprite,
+      border_N: y === 0,
+      border_E: x === img.bitmap.width - 1,
+      border_S: y === img.bitmap.height - 1,
+      border_W: x === 0
+    });
+  });
+
+  const vertices = [];
+  const linedefs = [];
+
+  pixels.forEach(pixel => {
+    // create vertices and linedefs for all pixels
+
+    let needs_N = false;
+    let needs_E = false;
+    let needs_S = false;
+    let needs_W = false;
+
+    let pxl_N = getPixel(pixel.x, pixel.y - 1);
+    let pxl_E = getPixel(pixel.x + 1, pixel.y);
+    let pxl_S = getPixel(pixel.x, pixel.y + 1);
+    let pxl_W = getPixel(pixel.x - 1, pixel.y);
+
+    if (pxl_N === undefined || pxl_N.lin_S === undefined) {
+      needs_N = true;
+    }
+    if (pxl_S === undefined || pxl_S.lin_N === undefined) {
+      needs_S = true;
+    }
+    if (pxl_E === undefined || pxl_E.lin_W === undefined) {
+      needs_E = true;
+    }
+    if (pxl_W === undefined || pxl_W.lin_E === undefined) {
+      needs_W = true;
+    }
+
+    const O = 1 * (SCALE * 0.5 * 1);
+
+    if (needs_N) {
+      const v0 = new Vertex(pixel.XS - O, pixel.YS + O);
+      const v1 = new Vertex(pixel.XS + O, pixel.YS + O);
+      const line = new Linedef(v0, v1);
+      pixel.lin_N = line;
+      pixel.own_N = true;
+      if (pxl_N) {
+        pxl_N.lin_S = line;
+        pxl_N.own_S = false;
+      }
+    }
+
+    if (needs_E) {
+      const v0 = new Vertex(pixel.XS + O, pixel.YS + O);
+      const v1 = new Vertex(pixel.XS + O, pixel.YS - O);
+      const line = new Linedef(v0, v1);
+      pixel.lin_E = line;
+      pixel.own_E = true;
+      if (pxl_E) {
+        pxl_E.lin_W = line;
+        pxl_E.own_W = false;
+      }
+    }
+
+    if (needs_S) {
+      const v0 = new Vertex(pixel.XS + O, pixel.YS - O);
+      const v1 = new Vertex(pixel.XS - O, pixel.YS - O);
+      const line = new Linedef(v0, v1);
+      pixel.lin_S = line;
+      pixel.own_S = true;
+      if (pxl_S) {
+        pxl_S.lin_N = line;
+        pxl_S.own_N = false;
+      }
+    }
+
+    if (needs_W) {
+      const v0 = new Vertex(pixel.XS - O, pixel.YS - O);
+      const v1 = new Vertex(pixel.XS - O, pixel.YS + O);
+      const line = new Linedef(v0, v1);
+      pixel.lin_W = line;
+      pixel.own_W = true;
+      if (pxl_W) {
+        pxl_W.lin_E = line;
+        pxl_W.own_E = false;
+      }
+    }
+  });
+
+  pixels.forEach(pixel => {
+    // create sectors and sidedefs for all pxels
+    pixel.sector = new Sector(pixel.sprite, 10 * SCALE, 1 * SCALE);
+    pixel.sid_N = new Sidedef(pixel.sector, pixel.sprite);
+    pixel.sid_E = new Sidedef(pixel.sector, pixel.sprite);
+    pixel.sid_S = new Sidedef(pixel.sector, pixel.sprite);
+    pixel.sid_W = new Sidedef(pixel.sector, pixel.sprite);
+
+    if (pixel.own_N) {
+      pixel.lin_N.sidefront = pixel.sid_N;
+    } else {
+      getPixel(pixel.x, pixel.y - 1).lin_S.sideback = pixel.sid_N;
+    }
+
+    if (pixel.own_E) {
+      pixel.lin_E.sidefront = pixel.sid_E;
+    } else {
+      getPixel(pixel.x + 1, pixel.y).lin_W.sideback = pixel.sid_E;
+    }
+
+    if (pixel.own_S) {
+      pixel.lin_S.sidefront = pixel.sid_S;
+    } else {
+      getPixel(pixel.x, pixel.y + 1).lin_N.sideback = pixel.sid_S;
+    }
+
+    if (pixel.own_W) {
+      pixel.lin_W.sidefront = pixel.sid_W;
+    } else {
+      getPixel(pixel.x - 1, pixel.y).lin_E.sideback = pixel.sid_W;
+    }
+
+    if (pixel.border_N) {
+      pixel.lin_N.sideback = new Sidedef(sector0, pixel.sprite); // correct sprite?
+    }
+    if (pixel.border_E) {
+      pixel.lin_E.sideback = new Sidedef(sector0, pixel.sprite); // correct sprite?
+    }
+    if (pixel.border_S) {
+      pixel.lin_S.sideback = new Sidedef(sector0, pixel.sprite); // correct sprite?
+    }
+    if (pixel.border_W) {
+      pixel.lin_W.sideback = new Sidedef(sector0, pixel.sprite); // correct sprite?
+    }
+  });
+
+  const udmfText = generateUDMF();
+  const w = WAD.WAD.read(fs.readFileSync("./base.wad"));
+  const mapLump = w.lumps.find(l => l.name === "TEXTMAP");
+  mapLump.data = Buffer.from(udmfText, "utf8");
+  fs.writeFileSync("test.wad", w.write());
+};
+
+Jimp.read("./lvl.png")
+  .then(async img => {
+    const colors = new Set();
+    img.scanQuiet(0, 0, img.bitmap.width, img.bitmap.height, (x, y, idx) => {
+      const color = img.getPixelColor(x, y);
+      colors.add(color);
+    });
+
+    await Promise.all(
+      Array.from(colors).map((color: number) => {
+        Jimp.create(16, 16, color)
+          .then(img => {
+            return img.writeAsync(`./SPRITES/${getSpriteName(color)}.png`);
+          })
+          .catch(console.error);
+      })
+    );
+
+    generateStuffFromimage(img);
+  })
+  .catch(console.error);
+
+const SCALE = 32;
+const generateRoom = (
+  v0: Vertex,
+  v1: Vertex,
+  v2: Vertex,
+  v3: Vertex,
+  texture: string,
+  heightfloor?: number
+) => {
+  const s0 = new Sector(texture, 10 * SCALE, -2 * SCALE);
+  const side1 = new Sidedef(s0, texture);
+  const side2 = new Sidedef(s0, texture);
+  const side3 = new Sidedef(s0, texture);
+  const side4 = new Sidedef(s0, texture);
+
+  const line1 = new Linedef(v0, v1, side1);
+  const line2 = new Linedef(v1, v2, side2);
+  const line3 = new Linedef(v2, v3, side3);
+  const line4 = new Linedef(v3, v0, side4);
+
+  return s0;
+};
---
+++ udmf_types.ts
@@ -0,0 +1,198 @@
+interface Serializable {
+  serialize: () => string;
+}
+
+interface Reffable {
+  ref: number;
+}
+
+interface UDMFObject extends Serializable, Reffable {}
+
+const n = (num: number) => {
+  return num.toFixed(3);
+};
+
+export enum RefType {
+  VERTEX = "VERTEX",
+  LINEDEF = "LINEDEF",
+  SECTOR = "SECTOR",
+  SIDEDEF = "SIDEDEF"
+}
+
+export const reffables: Map<RefType, UDMFObject[]> = new Map();
+const mkRef = (type: RefType, r: UDMFObject) => {
+  const exist = reffables.get(type);
+  let refs = [];
+  if (exist) {
+    refs = exist;
+  }
+  refs.push(r);
+  reffables.set(type, refs);
+};
+
+const getRef = (type: RefType, r: UDMFObject) => {
+  return reffables.get(type).indexOf(r);
+};
+
+////////
+
+export class Vertex implements UDMFObject {
+  constructor(public x: number, public y: number) {
+    const existing = Array.from(reffables.get(RefType.VERTEX) || []).find(
+      (v: Vertex) => {
+        v.x === x && v.y === y;
+      }
+    );
+    if (existing) {
+      return existing as Vertex;
+    }
+    mkRef(RefType.VERTEX, this);
+  }
+
+  get ref() {
+    return getRef(RefType.VERTEX, this);
+  }
+
+  serialize() {
+    return `
+// vertex ${this.ref}
+vertex
+{
+x = ${n(this.x)};
+y = ${n(this.y)};
+}
+
+`;
+  }
+}
+
+export class Linedef implements UDMFObject {
+  constructor(
+    public v1: Vertex,
+    public v2: Vertex,
+    public sidefront?: Sidedef,
+    public sideback?: Sidedef
+  ) {
+    mkRef(RefType.LINEDEF, this);
+  }
+
+  get ref() {
+    return getRef(RefType.LINEDEF, this);
+  }
+
+  serialize() {
+    return `
+// linedef ${this.ref}
+linedef
+{
+v1 = ${this.v1.ref};
+v2 = ${this.v2.ref};
+${this.sidefront ? `sidefront = ${this.sidefront.ref};` : ""}
+${this.sideback ? `sideback = ${this.sideback.ref};` : ""}
+${this.sideback && this.sidefront ? "twosided = true;" : ""}
+}
+    `;
+  }
+}
+
+export class Sidedef implements UDMFObject {
+  constructor(public sector: Sector, public texturemiddle?: string) {
+    mkRef(RefType.SIDEDEF, this);
+  }
+
+  get ref() {
+    return getRef(RefType.SIDEDEF, this);
+  }
+
+  serialize() {
+    return `
+// sidedef ${this.ref}
+sidedef
+{
+sector = ${this.sector.ref};
+${this.texturemiddle ? `texturemiddle = "${this.texturemiddle}";` : ""}
+${this.texturemiddle ? `texturebottom = "${this.texturemiddle}";` : ""}
+}
+    `;
+  }
+}
+
+export class Sector implements UDMFObject {
+  constructor(
+    public texturefloor: string,
+    public heightceiling: number,
+    public heightfloor: number
+  ) {
+    mkRef(RefType.SECTOR, this);
+  }
+
+  get ref() {
+    return getRef(RefType.SECTOR, this);
+  }
+
+  serialize() {
+    return `
+// sector ${this.ref}
+sector
+{
+texturefloor = "${this.texturefloor}";
+textureceiling = "0000";
+heightceiling = ${this.heightceiling};
+heightfloor = ${this.heightfloor};
+}
+`;
+  }
+}
+
+export const generateUDMF = () => {
+  return `
+namespace = "zdoom";
+thing
+{
+x = 96.000;
+y = -32.000;
+type = 1;
+skill1 = true;
+skill2 = true;
+skill3 = true;
+skill4 = true;
+skill5 = true;
+skill6 = true;
+skill7 = true;
+skill8 = true;
+single = true;
+coop = true;
+dm = true;
+class1 = true;
+class2 = true;
+class3 = true;
+class4 = true;
+class5 = true;
+}
+
+// Vertexes
+${reffables
+  .get(RefType.VERTEX)
+  .map(v => v.serialize())
+  .join("\n")}
+
+// Linedefs
+${reffables
+  .get(RefType.LINEDEF)
+  .map(v => v.serialize())
+  .join("\n")}
+
+// Sidedefs
+${reffables
+  .get(RefType.SIDEDEF)
+  .map(v => v.serialize())
+  .join("\n")}
+
+// Sectors
+${reffables
+  .get(RefType.SECTOR)
+  .map(v => v.serialize())
+  .join("\n")}
+  
+  `;
+};
